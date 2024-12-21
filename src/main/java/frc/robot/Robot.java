81fe7f51c60111cbd7c8afd9205d375840945f86
lddtwo456 <lddtwo45@gmail.com>
2024-12-20 21:07:50 -0500

    built in robot classes fixed up, configured bindings, configured default commands

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,18 +4,33 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+  /** Robot container */
+  private RobotContainer robotContainer;
+
+  /** Command to run during autonomous */
+  private Command autonomousCommand;
+
+  /**
+   * Returns true if on red alliance
+   * 
+   * @return true if on red alliance
+   */
+  public static boolean isRedAlliance() {
+    return DriverStation.getAlliance().isPresent()
+      && DriverStation.getAlliance().get() == Alliance.Red;
+  }
 
   @Override
   public void robotInit() {
-    m_robotContainer = new RobotContainer();
+    robotContainer = RobotContainer.getInstance();
   }
 
   @Override
@@ -34,10 +49,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    autonomousCommand = robotContainer.getAutonomousCommand();
 
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
     }
   }
 
@@ -49,8 +64,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    if (autonomousCommand != null) {
+      autonomousCommand.cancel();
     }
   }
 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,64 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.odometry.Odometry;
+import frc.robot.swerve.Swerve;
 
+/** Robot container */
 public class RobotContainer {
-  public RobotContainer() {
+
+  /** Robot container singleton */
+  private static RobotContainer instance = null;
+
+  /** Odometry subsystem reference */
+  private final Odometry odometry;
+
+  /** Swerve subsystem reference */
+  private final Swerve swerve;
+
+  /** Driver controller */
+  private final CommandXboxController driverController;
+
+  /** Operator controller */
+  private final CommandXboxController operatorController;
+
+  /** Initializes the robot container */
+  private RobotContainer() {
+    odometry = Odometry.getInstance();
+    swerve = Swerve.getInstance();
+
+    driverController = new CommandXboxController(0);
+    operatorController = new CommandXboxController(1);
+
+    configureDefaultCommands();
     configureBindings();
   }
 
-  private void configureBindings() {}
+  /**
+   * Returns the robot container
+   * 
+   * @return the robot container
+   */
+  public static RobotContainer getInstance() {
+    if (instance == null) {
+      instance = new RobotContainer();
+    }
+
+    return instance;
+  }
+
+  /** Configures subsystem default commands */
+  private void configureDefaultCommands() {
+    swerve.setDefaultCommand(swerve.teleopDrive(driverController));
+  }
+
+  /** Configures controller bindings */
+  private void configureBindings() {
+    driverController.y().onTrue(odometry.zeroYaw());
+  }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return Commands.print("No autonomous commands configured");
   }
 }
