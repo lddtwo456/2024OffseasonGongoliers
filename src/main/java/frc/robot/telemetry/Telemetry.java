d07e09b3215d5cd9fc4942a9e3b054bc170f62aa
lddtwo456 <lddtwo45@gmail.com>
2024-12-23 21:45:18 -0500

    javadoc

--- src/main/java/frc/robot/telemetry/Telemetry.java
+++ src/main/java/frc/robot/telemetry/Telemetry.java
@@ -9,20 +9,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 /** Telemetry with shuffleboard */
 public class Telemetry extends SubsystemBase {
   
+  /** Telemetry singleton */
   private static Telemetry instance = null;
 
+  /** Hashmaps of telemetry values for shuffleboard value suppliers */
   private HashMap<String, Double> doubles = new HashMap<String, Double>();
   private HashMap<String, Boolean> bools = new HashMap<String, Boolean>();
 
+  /** Lists of value updating runnables */
   private ArrayList<Runnable> even_updaters = new ArrayList<Runnable>();
   private ArrayList<Runnable> odd_updaters = new ArrayList<Runnable>();
 
+  /** Switches between 1 and 0 every periodic loop to switch between the two groups of updaters to be called */
   private int periodic_loop;
 
+  /** Telemetry subsystem constructor */
   private Telemetry() {
     periodic_loop = 0;
   }
 
+  /** Gets telemetry subsystem instance */
   public Telemetry getInstance() {
     if (instance == null) {
       instance = new Telemetry();
@@ -31,6 +37,7 @@ public class Telemetry extends SubsystemBase {
     return instance;
   }
 
+  /** Runs every periodic loop, updating telemetry values in the hashmaps */
   @Override
   public void periodic() {
     periodic_loop ^= 1;
@@ -46,14 +53,32 @@ public class Telemetry extends SubsystemBase {
     }
   }
 
+  /**
+   * Gets the value of a double in the hashmap of doubles
+   * 
+   * @param name name of value in hashmap
+   * @return the value of a double in the hashmap of doubles
+   */
   public double getDouble(String name) {
     return doubles.getOrDefault(name, 0.0);
   }
 
+  /**
+   * Gets the value of a bool in the hashmap of bools
+   * 
+   * @param name name of value in hashmap
+   * @return the value of a bool in the hashmap of bools
+   */
   public boolean getBool(String name) {
     return bools.getOrDefault(name, false);
   }
 
+  /**
+   * Add a double value along with its updater
+   * 
+   * @param name name of double value
+   * @param supplier function that supplies the double's updated value
+   */
   public void addDouble(String name, Supplier<Double> supplier) {
     even_updaters.add(
       () -> {
@@ -61,6 +86,12 @@ public class Telemetry extends SubsystemBase {
       });
   }
 
+  /**
+   * Add a boolean value along with its updater
+   * 
+   * @param name name of the bool value
+   * @param supplier function that supplies the bool's updated value
+   */
   public void addBool(String name, Supplier<Boolean> supplier) {
     even_updaters.add(
       () -> {
@@ -68,6 +99,7 @@ public class Telemetry extends SubsystemBase {
       });
   }
 
+  /** Staggers the added updaters between list of updaters called on odd and even periodic loops */
   public void staggerUpdaters() {
     int split_index = even_updaters.size() / 2;
 
